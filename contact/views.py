1642a3d9e89e0b0b3924e3939897e334b3efe05d
Madison Evans <m.corbinevans@gmail.com>
2023-03-02 11:56:02 -0700

    checkpoint before configuring the endpoints

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Contact
+from .serializers import ContactSerializer
+
+@api_view(['GET', 'POST'])
+def contact(request):
+    
+    if request.method == 'GET':
+        return Response({'message': 'Hello from the contact API endpoint', 'uri': request.build_absolute_uri()})
+    
+    elif request.method == 'POST':
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
