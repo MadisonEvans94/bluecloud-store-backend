98aaf76f9d6cfe36c149401096386e9f34912113
Madison Evans <m.corbinevans@gmail.com>
2023-03-02 12:17:22 -0700

    updates added to contact model

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contact'
+    'contact', 
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- contact/models.py
+++ contact/models.py
@@ -2,9 +2,9 @@ from django.db import models
 class Contact(models.Model):
     full_name = models.CharField(max_length=255)
     email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    # phone_number = models.CharField(max_length=20)
+    # message = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.full_name
