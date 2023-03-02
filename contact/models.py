490989f6321c794728df54641605f31fc9be5da1
Madison Evans <m.corbinevans@gmail.com>
2023-03-02 11:28:12 -0700

    added migrations

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Contact(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.full_name
